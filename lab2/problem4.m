71372575254e8b9ca1a9e5b21f2836da10d0868b
github-private <pontus.linde@me.com>
2023-05-04 09:05:07 +0200

    added necessary stuff

--- lab2/problem4.m
+++ lab2/problem4.m
@@ -1,15 +1,44 @@
 %% Problem 4: Fordelningar av givna data
 load birth.dat
-x = birth(birth(:, 20) < 3, 3);
-y = birth(birth(:, 20) == 3, 3);
 
-subplot(2,2,1), boxplot(x),
-axis([0 2 500 5000])
-subplot(2,2,2), boxplot(y),
-axis([0 2 500 5000])
+% Vill plotta en figur med fyra olika histogram som visar fördelningarna för barnets födelsevikt, moderns ålder, moderns längd respektive moderns vikt.
+% Barnets vikt - 3
+% Moderns ålder - 4
+% Moderns längd - 16
+% Moderns vikt - 15
 
-subplot(2,2,3:4), ksdensity(x),
-hold on
-[fy, ty] = ksdensity(y);
-plot(ty, fy, 'r')
-hold off
+barnVikt = birth(:, 3);
+moderAlder = birth(:, 4);
+moderLangd = birth(:, 16);
+moderVikt = birth(:, 15);
+
+
+subplot(2,1,2), hist_density(barnVikt)
+title('Barn Vikt')
+
+subplot(2,1,2), hist_density(moderAlder),
+title('Moder Alder')
+
+subplot(2,1,2), hist_density(moderLangd),
+title('Moder Langd')
+
+subplot(2,1,2), hist_density(moderVikt),
+title('Moder Vikt')
+
+
+
+% x = birth(birth(:, 20) < 3, 3);
+% y = birth(birth(:, 20) == 3, 3);
+
+% % Problem 4: Fordelningar av givna data (forts.)
+% subplot(2,2,1), boxplot(x),
+% axis([0 2 500 5000])
+% subplot(2,2,2), boxplot(y),
+% axis([0 2 500 5000])
+
+% %% Problem 4: Fordelningar av givna data (forts.)
+% subplot(2,2,3:4), ksdensity(x),
+% hold on
+% [fy, ty] = ksdensity(y);
+% plot(ty, fy, 'r')
+% hold off
